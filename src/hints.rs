01207078e5d5327251ea575f68f263d8f4a0c8ab
rusconn <rusconn3@gmail.com>
2024-02-16 12:06:52 +0900

    refactor: add Hints type and move to main crate

---
+++ src/hints.rs
@@ -0,0 +1,37 @@
+mod hint;
+
+use std::collections::HashSet;
+
+use wordler::letter_infos::LetterInfos;
+
+use self::hint::{Hint, UnknownHintError};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Hints(Vec<Hint>);
+
+impl TryFrom<&str> for Hints {
+    type Error = UnknownHintError;
+
+    fn try_from(hints: &str) -> Result<Self, Self::Error> {
+        hints
+            .chars()
+            .map(Hint::try_from)
+            .collect::<Result<_, _>>()
+            .map(Hints)
+    }
+}
+
+impl Hints {
+    pub fn apply(
+        &self,
+        word: &str,
+        letter_infos: &mut LetterInfos,
+        contains: &mut HashSet<char>,
+        not_contains: &mut HashSet<char>,
+        unuseds: &mut HashSet<char>,
+    ) {
+        for (nth, (letter, hint)) in word.chars().zip(&self.0).enumerate() {
+            hint.apply(nth, letter, letter_infos, contains, not_contains, unuseds);
+        }
+    }
+}

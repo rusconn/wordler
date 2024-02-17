b9d982764bd017867833a09ca005ccf6b7a64591
rusconn <rusconn3@gmail.com>
2024-02-17 23:20:03 +0900

    perf: use rustc-hash

--- src/excludes.rs
+++ src/excludes.rs
@@ -1,16 +1,16 @@
-use std::collections::HashSet;
+use rustc_hash::FxHashSet;
 
 use crate::Letter;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Excludes(HashSet<Letter>);
+pub struct Excludes(FxHashSet<Letter>);
 
 impl Excludes {
     pub fn new() -> Self {
         Self(Default::default())
     }
 
-    pub fn is_disjoint(&self, other: &HashSet<Letter>) -> bool {
+    pub fn is_disjoint(&self, other: &FxHashSet<Letter>) -> bool {
         self.0.is_disjoint(other)
     }
 
--- src/veileds.rs
+++ src/veileds.rs
@@ -1,14 +1,14 @@
-use std::collections::HashSet;
+use rustc_hash::FxHashSet;
 
 use crate::Letter;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Veileds(HashSet<Letter>);
+pub struct Veileds(FxHashSet<Letter>);
 
 impl Veileds {
     /// Make sure the chars are valid
     pub fn unsafe_from<T: Iterator<Item = char>>(chars: T) -> Self {
-        Self(HashSet::from_iter(chars.map(Letter::unsafe_from)))
+        Self(FxHashSet::from_iter(chars.map(Letter::unsafe_from)))
     }
 
     pub fn contains(&self, letter: &Letter) -> bool {

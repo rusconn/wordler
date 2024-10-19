1f720300a4615d67eb5e7710639e28f484093b6c
rusconn <rusconn3@gmail.com>
2024-10-19 13:58:15 +0900

    refactor: from_iter -> collect

--- src/letter_set/veileds.rs
+++ src/letter_set/veileds.rs
@@ -1,12 +1,12 @@
 use derive_more::derive::{Deref, DerefMut};
 
-use super::{Letter, LetterSet, Set};
+use super::{Letter, LetterSet};
 
 #[derive(Debug, Clone, PartialEq, Eq, Deref, DerefMut)]
 pub struct Veileds(LetterSet);
 
 impl Default for Veileds {
     fn default() -> Self {
-        Self(Set::from_iter((b'A'..=b'Z').map(Letter::from_unchecked)))
+        Self((b'A'..=b'Z').map(Letter::from_unchecked).collect())
     }
 }

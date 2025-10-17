51cad2c98edd357d87eecc07e3da62c5ddcb1d01
rusconn <rusconn3@gmail.com>
2025-10-17 20:31:29 +0900

    refactor: use thiserror

--- src/bin/cli/main.rs
+++ src/bin/cli/main.rs
@@ -1,3 +1,5 @@
+mod error;
+
 use std::{
     io::{self, Stdin, Stdout, Write},
     str::FromStr,
@@ -5,10 +7,9 @@ use std::{
 
 use itertools::Itertools;
 
-use wordler::{
-    Candidates, Guess, Hints, ParseGuessError, ParseHintError, ParseHintsError, ParseLetterError,
-    Recommends, State,
-};
+use wordler::{Candidates, Guess, Hints, Recommends, State};
+
+use self::error::ParseError;
 
 fn main() {
     let mut state = State::default();
@@ -54,20 +55,18 @@ fn show_recommends(recommends: &Recommends) -> String {
 }
 
 fn read_guess(stdin: &Stdin, stdout: &mut Stdout) -> Guess {
-    read_line(stdin, stdout, "Guess", "guess", show_parse_guess_error)
+    read_line(stdin, stdout, "Guess", "guess")
 }
 
 fn read_hints(stdin: &Stdin, stdout: &mut Stdout) -> Hints {
-    read_line(stdin, stdout, "Hints", "hints", show_parse_hints_error)
+    read_line(stdin, stdout, "Hints", "hints")
 }
 
-fn read_line<T: FromStr>(
-    stdin: &Stdin,
-    stdout: &mut Stdout,
-    label: &str,
-    kind: &str,
-    show_parse_error: impl Fn(<T as FromStr>::Err) -> String,
-) -> T {
+fn read_line<T>(stdin: &Stdin, stdout: &mut Stdout, label: &str, kind: &str) -> T
+where
+    T: FromStr,
+    ParseError: From<<T>::Err>,
+{
     loop {
         print!("{label}: ");
         stdout.flush().unwrap();
@@ -76,7 +75,7 @@ fn read_line<T: FromStr>(
 
         match input.parse::<T>() {
             Ok(t) => return t,
-            Err(e) => eprintln!("Failed to read the {kind}: {}", show_parse_error(e)),
+            Err(e) => eprintln!("Failed to read the {kind}: {}", ParseError::from(e)),
         }
     }
 }
@@ -85,33 +84,4 @@ fn get_line(stdin: &Stdin) -> String {
     let mut buf = String::new();
     stdin.read_line(&mut buf).unwrap();
     buf.trim().into()
-}
-
-fn show_parse_guess_error(e: ParseGuessError) -> String {
-    match e {
-        ParseGuessError::InvalidLength => "Guess must be 5 letters".into(),
-        ParseGuessError::UnknownWord => "Unknown word".into(),
-        ParseGuessError::InvalidLetter(e) => show_parse_letter_error(e),
-    }
-}
-
-fn show_parse_letter_error(e: ParseLetterError) -> String {
-    match e {
-        ParseLetterError::NonAlphabeticalLetter(c) => {
-            format!("Non alphabetical letter: `{c}`")
-        }
-    }
-}
-
-fn show_parse_hints_error(e: ParseHintsError) -> String {
-    match e {
-        ParseHintsError::InvalidLength => "Hints must be 5 letters".into(),
-        ParseHintsError::InvalidHint(e) => show_parse_hint_error(e),
-    }
-}
-
-fn show_parse_hint_error(e: ParseHintError) -> String {
-    match e {
-        ParseHintError::InvalidHint(c) => format!("Invalid hint: `{c}`"),
-    }
 }

f46a33cb56f797c1677e9613d607878777ce40b6
rusconn <rusconn3@gmail.com>
2026-05-12 17:41:34 +0900

    change(lib)!: make recommendation feature a feature flag

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,7 +1,12 @@
 mod dict;
 mod input;
 mod letter;
-mod recommends;
 mod state;
 
-pub use {input::*, recommends::*, state::*};
+pub use {input::*, state::*};
+
+#[cfg(feature = "recommend")]
+mod recommends;
+
+#[cfg(feature = "recommend")]
+pub use recommends::*;
